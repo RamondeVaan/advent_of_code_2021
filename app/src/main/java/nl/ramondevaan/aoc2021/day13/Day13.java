9252857a66bf2bd005962941d3866773c4f03074
Ramon de Vaan <ramon.de.vaan@outlook.com>
2021-12-12 22:06:59 +0100

    Prepare day 13

---
+++ app/src/main/java/nl/ramondevaan/aoc2021/day13/Day13.java
@@ -0,0 +1,18 @@
+package nl.ramondevaan.aoc2021.day13;
+
+import java.util.List;
+
+public class Day13 {
+
+    public Day13(List<String> lines) {
+
+    }
+
+    public long solve1() {
+        return 0L;
+    }
+
+    public long solve2() {
+        return 0L;
+    }
+}
