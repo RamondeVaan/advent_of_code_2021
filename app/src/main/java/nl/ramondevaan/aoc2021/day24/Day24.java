b51e2ffb1568571969d192b59f932a0736ea77a2
Ramon de Vaan <ramon.de.vaan@outlook.com>
2021-12-23 20:50:28 +0100

    Prepare day 24

---
+++ app/src/main/java/nl/ramondevaan/aoc2021/day24/Day24.java
@@ -0,0 +1,18 @@
+package nl.ramondevaan.aoc2021.day24;
+
+import java.util.List;
+
+public class Day24 {
+
+    public Day24(List<String> lines) {
+
+    }
+
+    public long solve1() {
+        return 0L;
+    }
+
+    public long solve2() {
+        return 0L;
+    }
+}
